730962bdba81dae9963efd9ef3c4591393119855
rhorton5 <48656566+rhorton5@users.noreply.github.com>
2021-11-12 11:45:32 -0800

    Initial Commit

---
+++ src/Medical.cxx
@@ -0,0 +1,142 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    Medical1.cxx
+
+  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+//
+// This example reads a volume dataset, extracts an isosurface that
+// represents the skin and displays it.
+//
+
+#include <vtkRenderer.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkVolume16Reader.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkActor.h>
+#include <vtkOutlineFilter.h>
+#include <vtkCamera.h>
+#include <vtkProperty.h>
+#include <vtkPolyDataNormals.h>
+#include <vtkContourFilter.h>
+#include <vtkSmartPointer.h>
+#include <vtkStructuredPointsReader.h>
+
+
+int main (int argc, char *argv[])
+{
+
+  // Create the renderer, the render window, and the interactor. The renderer
+  // draws into the render window, the interactor enables mouse- and
+  // keyboard-based interaction with the data within the render window.
+  //
+  vtkSmartPointer<vtkRenderer> aRenderer =
+    vtkSmartPointer<vtkRenderer>::New();
+  vtkSmartPointer<vtkRenderWindow> renWin =
+    vtkSmartPointer<vtkRenderWindow>::New();
+  renWin->AddRenderer(aRenderer);
+
+  vtkSmartPointer<vtkRenderWindowInteractor> iren =
+    vtkSmartPointer<vtkRenderWindowInteractor>::New();
+  iren->SetRenderWindow(renWin);
+
+  
+
+  // The following reader is used to read a series of 2D slices (images)
+  // that compose the volume. The slice dimensions are set, and the
+  // pixel spacing. The data Endianness must also be specified. The reader
+  // uses the FilePrefix in combination with the slice number to construct
+  // filenames using the format FilePrefix.%d. (In this case the FilePrefix
+  // is the root name of the file: quarter.)
+  vtkSmartPointer<vtkStructuredPointsReader> v16 =
+      vtkSmartPointer<vtkStructuredPointsReader>::New();
+  v16->SetFileName("temperature.dat");
+  v16->Update();
+
+  // An isosurface, or contour value of 500 is known to correspond to the
+  // skin of the patient. Once generated, a vtkPolyDataNormals filter is
+  // is used to create normals for smooth surface shading during rendering.
+  vtkSmartPointer<vtkContourFilter> skinExtractor =
+    vtkSmartPointer<vtkContourFilter>::New();
+  skinExtractor->SetInputConnection(v16->GetOutputPort());
+  skinExtractor->SetValue(0, 0.10);
+
+  vtkSmartPointer<vtkPolyDataNormals> skinNormals =
+    vtkSmartPointer<vtkPolyDataNormals>::New();
+  skinNormals->SetInputConnection(skinExtractor->GetOutputPort());
+  skinNormals->SetFeatureAngle(60.0);
+
+  vtkSmartPointer<vtkPolyDataMapper> skinMapper =
+    vtkSmartPointer<vtkPolyDataMapper>::New();
+  skinMapper->SetInputConnection(skinNormals->GetOutputPort());
+  //skinMapper->ScalarVisibilityOff();
+  skinMapper->ScalarVisibilityOn();
+
+  vtkSmartPointer<vtkActor> skin =
+    vtkSmartPointer<vtkActor>::New();
+  skin->SetMapper(skinMapper);
+
+  // An outline provides context around the data.
+  //
+  vtkSmartPointer<vtkOutlineFilter> outlineData =
+    vtkSmartPointer<vtkOutlineFilter>::New();
+  outlineData->SetInputConnection(v16->GetOutputPort());
+
+  vtkSmartPointer<vtkPolyDataMapper> mapOutline =
+    vtkSmartPointer<vtkPolyDataMapper>::New();
+  mapOutline->SetInputConnection(outlineData->GetOutputPort());
+
+  vtkSmartPointer<vtkActor> outline =
+    vtkSmartPointer<vtkActor>::New();
+  outline->SetMapper(mapOutline);
+  outline->GetProperty()->SetColor(0,0,0);
+
+  // It is convenient to create an initial view of the data. The FocalPoint
+  // and Position form a vector direction. Later on (ResetCamera() method)
+  // this vector is used to position the camera to look at the data in
+  // this direction.
+  vtkSmartPointer<vtkCamera> aCamera =
+    vtkSmartPointer<vtkCamera>::New();
+  aCamera->SetViewUp(0, 1, 0);
+  aCamera->SetPosition(0, 0, 1);
+  aCamera->Azimuth(0.0);
+  aCamera->Elevation(0.0);
+  aCamera->Dolly(1.0);
+
+  // Actors are added to the renderer. An initial camera view is created.
+  // The Dolly() method moves the camera towards the FocalPoint,
+  // thereby enlarging the image.
+  aRenderer->AddActor(outline);
+  aRenderer->AddActor(skin);
+  aRenderer->SetActiveCamera(aCamera);
+  aRenderer->ResetCamera ();
+  aCamera->Dolly(1.5);
+
+  // Set a background color for the renderer and set the size of the
+  // render window (expressed in pixels).
+  aRenderer->SetBackground(.2, .3, .4);
+  renWin->SetSize(640, 480);
+
+  // Note that when camera movement occurs (as it does in the Dolly()
+  // method), the clipping planes often need adjusting. Clipping planes
+  // consist of two planes: near and far along the view direction. The
+  // near plane clips out objects in front of the plane; the far plane
+  // clips out objects behind the plane. This way only what is drawn
+  // between the planes is actually rendered.
+  aRenderer->ResetCameraClippingRange ();
+
+  // Initialize the event loop and then start it.
+  iren->Initialize();
+  iren->Start();
+
+  return EXIT_SUCCESS;
+}
